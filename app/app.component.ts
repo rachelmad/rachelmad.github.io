290244ef92e21bfe2c6992e13cd0e20acbdb6ade
rachelmad <rachelvmadrigal@gmail.com>
2016-07-31 01:20:29 -0500

    Implemented Angular 2

---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'app',
+    template: '<p>This is test text to see if Angular 2 will work on Github pages</p>'
+})
+
+export class AppComponent { }
+
